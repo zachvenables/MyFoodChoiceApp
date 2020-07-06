1bdb90da08b0f2711e3a77e973be8eacf096127b
zachvenables <venables.3@buckeyemail.osu.edu>
2020-07-06 16:42:08 -0400

    had to add idea files back in

--- screens/NearestFoodScreen.js
+++ screens/NearestFoodScreen.js
@@ -17,45 +17,10 @@ class NearestFoodScreen extends React.Component {
 	
 	constructor(props) {
 		super (props);
-
-		
-
-
-		/*
-		//Initialize Firebase..
-		if(!firebase.apps.length){
-			firebase.initializeApp({
-					apiKey: "AIzaSyBCjwYHTf9Yj1kAN7mByIhnA3rD0OZlzJY",
-					authDomain: "osumyfoodchoiceapp-a8fd6.firebaseapp.com",
-					databaseURL: "https://osumyfoodchoiceapp-a8fd6.firebaseio.com",
-					projectId: "osumyfoodchoiceapp-a8fd6",
-					storageBucket: "osumyfoodchoiceapp-a8fd6.appspot.com",
-					messagingSenderId: "752614312654",
-					appId: "1:752614312654:web:e3234a1c1c83e85a0dde9f",
-					measurementId: "G-XKCPW0Q23G"
-			});
-		}
-		
-		this.database = firebase.firestore();
-		*/
 		
 		this.locationName = "Eatery";
 
-		this.state = {
-		/*
-			foods: [
-				{'name': 'FoodA', 'calories': 100, 'id': 1},
-				{'name': 'FoodB', 'calories': 200, 'id': 2},
-				{'name': 'FoodC', 'calories': 300, 'id': 3},
-				{'name': 'FoodD', 'calories': 400, 'id': 4},
-				{'name': 'FoodE', 'calories': 100, 'id': 5},
-				{'name': 'FoodF', 'calories': 320, 'id': 6},
-				{'name': 'FoodG', 'calories': 500, 'id': 7},
-				{'name': 'FoodH', 'calories': 100, 'id': 8},
-				{'name': 'FoodI', 'calories': 300, 'id': 9}
-			]
-			*/
-		};
+		this.state = {};
 		
 		this.mealPlanCheck = this.mealPlanCheck.bind(this);
 		this.loadNextLocation = this.loadNextLocation.bind(this);
@@ -84,8 +49,6 @@ class NearestFoodScreen extends React.Component {
 
 	render() {
 		var { user, location, nextState } = this.props.route.params;
-
-		//alert(this.database.collection('location'));
 		
 		return(
 		<View style={{flex: 1, flexDirection: 'column', alignItems: 'stretch',}}>
